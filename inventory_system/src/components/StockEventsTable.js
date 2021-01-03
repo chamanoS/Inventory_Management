952dc7fafca07fd52434c44c98a4bd5820a65196
chamano sidney <chamano.sr@gmail.com>
2021-01-03 15:58:58 +0200

    display by id

--- inventory_system/src/components/StockEventsTable.js
+++ inventory_system/src/components/StockEventsTable.js
@@ -1,17 +1,30 @@
 import React from 'react'
 
 function StockEventsTable(props) {
-    const {stockEvents} = props
+    const {products, stockEvents} = props
+
         return (
             <div className="StockEventsTable">
-                {stockEvents.map(event =>(
-                    <div className="StockEventTable_Card">
-                        <p>Id:{event.id}</p>
-                        <p>Type:{event.type}</p>
-                        <p>Quantity:{event.qty}</p>
-                        <p>Product Name:{event.product.name}</p>
-                    </div>
-                ))}
+                {products.map(product => {
+                    const {id} = product
+
+                    const relevantStockEvents = stockEvents.filter(se => se.product.id === product.id)
+                    return(
+                        <div>
+                            <h2>Product: {product.name}</h2>
+                            {relevantStockEvents.map(event =>(
+                              <div className="StockEventTable_Card">
+                                <p>Id:{event.id}</p>
+                                <p>Type:{event.type}</p>
+                                <p>Quantity:{event.qty}</p>
+                                <p>Product Name:{event.product.name}</p>
+                              </div>
+                            ))}
+
+                        </div>
+                    )
+                })}
+               
             </div>
         )
 }
