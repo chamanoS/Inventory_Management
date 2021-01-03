f385d15439c386153786e0225ce6db23d6705e25
chamano sidney <chamano.sr@gmail.com>
2021-01-03 12:27:09 +0200

    seting up comp

---
+++ inventory_system/src/components/StockEventsTable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function StockEventsTable(props) {
+    const {stockEvents} = props
+        return (
+            <div className="StockEventsTable">
+                {[1,2,3]}
+            </div>
+        )
+}
+
+export default StockEventsTable;
