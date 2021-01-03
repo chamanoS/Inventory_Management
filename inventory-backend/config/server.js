9c1fa7e658a457b61a34cedd140c56fa8e05d0fe
chamano sidney <chamano.sr@gmail.com>
2021-01-03 10:15:10 +0200

    structure setup

---
+++ inventory-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2852fb4fdf4281def442fc1236e4b44e'),
+    },
+  },
+});
