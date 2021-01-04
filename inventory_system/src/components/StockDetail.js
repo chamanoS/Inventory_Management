4dc10f9795e18ad06084c9dc95caaaaa27eed16e
chamano sidney <chamano.sr@gmail.com>
2021-01-04 09:39:52 +0200

    toggling the state

--- inventory_system/src/components/StockDetail.js
+++ inventory_system/src/components/StockDetail.js
@@ -1,20 +1,28 @@
 import React from 'react'
 
-class StockDetail extends React.Component{
-
-    render(){
-        const {name, total, stockEvents} = this.props
+class StockDetail extends React.Component {
+    state = {
+        show: false
+    }
+    render() {
+        const { name, total, stockEvents } = this.props
+        const { show } = this.state
         return (
-            <div className="StockDetail">
+            <div className="StockDetail" onClick={() => this.setState({show:!show})}>
                 <h2>Product: {name} | Total: {total}</h2>
-                {stockEvents.map(event => (
-                    <div className="StockEventTable_Card">
-                        <p>Id:{event.id}</p>
-                        <p>Type:{event.type}</p>
-                        <p>Quantity:{event.qty}</p>
-                        <p>Product Name:{event.product.name}</p>
+                {show &&
+                    <div>
+                        {stockEvents.map(event => (
+                            <div className="StockEventTable_Card">
+                                <p>Id:{event.id}</p>
+                                <p>Type:{event.type}</p>
+                                <p>Quantity:{event.qty}</p>
+                                <p>Product Name:{event.product.name}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                }
+
             </div>
         )
     }
